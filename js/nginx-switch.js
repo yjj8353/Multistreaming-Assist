7e1980a36c0eb74d4ad8cd71cc8b4fed6684f273
yjj8353 <yjj8353@gmail.com>
2020-03-27 10:25:42 +0900

    view.html의 스크립트 일부를 nginx-switch.js로 옮김

--- js/nginx-switch.js
+++ js/nginx-switch.js
@@ -1,5 +1,10 @@
 // nginx.exe 실행
 exports.on = function() {
+    document.getElementById('switch_button').setAttribute('value', 'nginx 서버 정지');
+    document.getElementById('switch_button').setAttribute('class', 'btn btn-danger');
+    // main.js에 현재 niginx.exe가 on 되었다는 신호를 보냄
+    ipcRenderer.send('asynchronous-message', 'true');
+
     // nginx.exe를 실행하고, 에러가 있을경우 에러 정보를 리턴하고 nginx.exe 시작 전 상태로 돌아감
     execFile('./nginx.exe', {cwd: dir + "\\nginx"}, (error, stdout, stderr) => {
         if(error) {
@@ -19,12 +24,13 @@ exports.on = function() {
 
 // nginx.exe 중단
 exports.off = function () {
-    //nginx.exe를 종료하고, 에러가 있을경우 nginx.exe 시작 전 상태로 돌아감
+    document.getElementById('switch_button').setAttribute('value', 'nginx 서버 시작');
+    document.getElementById('switch_button').setAttribute('class', 'btn btn-primary');
+    // main.js에 현재 niginx.exe가 off 되었다는 신호를 보냄
+    ipcRenderer.send('asynchronous-message', 'false');
+
+    //nginx.exe를 종료하고, 에러가 있을경우 무시함
     execFileSync('./nginx.exe', ['-s', 'stop'], {cwd: dir + "\\nginx"}, (error, stdout, stderr) => {
-        if(error) {
-            document.getElementById('switch_button').setAttribute('value', 'nginx 서버 시작');
-            document.getElementById('switch_button').setAttribute('class', 'btn btn-primary');
-            ipcRenderer.send('asynchronous-message', 'false');
-        }
+        if(error) throw error;
     });
 }
