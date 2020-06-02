5357b97f15ed9287939734c3da28b1a2f4ef4038
yjj8353 <yjj8353@gmail.com>
2020-06-02 10:48:15 +0900

    주석 추가

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page-container>
     <q-page class="q-pa-md" style="background-color: white;">
-      <!-- 경로에 한글이 포함되어 있지 않을 경우 실행 -->
+      <!-- 경로에 한글이 포함되어 있지 않을 경우 -->
       <div v-if="checkIncludeKoreanOnPath()">
+
+        <!-- TWITCH RTMP KEY 입력 칸 -->
         <div class="row">
           <div class="col-11">
-            <q-input label="TWITCH RTMP KEY" v-model="twitchKey" :type="twitchIsPwd ? 'password' : 'text'" @input="checTwitchKeykNull">
+            <q-input label="TWITCH RTMP KEY" v-model="twitchKey" :type="twitchIsPwd ? 'password' : 'text'" @input="checkTwitchKeykNull">
               <template v-slot:append>
                 <q-icon
                   :name="twitchIsPwd ? 'visibility_off' : 'visibility'"
@@ -15,13 +17,16 @@
               </template>
             </q-input>
           </div>
+
+          <!-- TWITCH RTMP KEY 반영 토글 스위치 -->
           <div class="col-1" style="display: flex; align-items: center; justify-content: center;">
-            <q-toggle v-model="twitchOn" :disable="!twitchKey" size="md" @input="toggleSwitch" />
+            <q-toggle v-model="twitchOn" :disable="!twitchKey" size="md" @input="toggleSwitchIsToggle" />
           </div>
         </div>
 
         <p></p>
 
+        <!-- YOUTUBE RTMP KEY 입력 칸 -->
         <div class="row">
           <div class="col-11">
             <q-input label="YOUTUBE RTMP KEY" v-model="youtubeKey" :type="youtubeIsPwd ? 'password' : 'text'" @input="checkYoutubeKeyNull">
@@ -34,13 +39,16 @@
               </template>
             </q-input>
           </div>
+
+           <!-- YOUTUBE RTMP KEY 반영 토글 스위치 -->
           <div class="col-1" style="display: flex; align-items: center; justify-content: center;">
-            <q-toggle v-model="youtubeOn" :disable="!youtubeKey" size="md" @input="toggleSwitch" />
+            <q-toggle v-model="youtubeOn" :disable="!youtubeKey" size="md" @input="toggleSwitchIsToggle" />
           </div>
         </div>
 
         <p></p>
 
+        <!-- 추가적인 RTMP URL 입력 칸 -->
         <div class="row">
           <div class="q-pr-md col">
             <q-input label="추가적인 RTMP URL" v-model="additionalRTMPUrl" type="text" @input="checkAddtionalRTMPUrlOrKeyNull">
@@ -49,6 +57,7 @@
             </q-input>
           </div>
 
+          <!-- 추가적은 RTMP KEY 입력 칸 -->
           <div class="q-pl-md col">
             <q-input label="추가적인 RTMP KEY" v-model="additionalRTMPKey" :type="additionalIsPwd ? 'password' : 'text'" @input="checkAddtionalRTMPUrlOrKeyNull">
               <template v-slot:append>
@@ -60,8 +69,10 @@
               </template>
             </q-input>
           </div>
+
+           <!-- 추가적인 RTMP URL 및 KEY 반영 토글 스위치 -->
           <div class="col-1" style="display: flex; align-items: center; justify-content: center;">
-            <q-toggle v-model="additionalOn" :disable="!additionalRTMPUrl || !additionalRTMPKey" size="md" @input="toggleSwitch" />
+            <q-toggle v-model="additionalOn" :disable="!additionalRTMPUrl || !additionalRTMPKey" size="md" @input="toggleSwitchIsToggle" />
           </div>
         </div>
 
@@ -77,7 +88,7 @@
         </div>
       </div>
 
-      <!-- 경로에 한글이 포함되어 있을 경우 실행 -->
+      <!-- 경로에 한글이 포함되어 있을 경우 -->
       <div v-else>
         <q-card class="my-card">
           <q-card-section>
@@ -92,20 +103,32 @@
 </template>
 
 <script>
-import { startNginxProcess, quitNginxProcess, findNginxProcess, testNginxProcess } from '../function/nginx'
-import path from 'path'
 import fs from 'fs'
+import path from 'path'
 
-import { checkKey, makeNginxConfFile, makeRTMPJSON, makeRTMPJSONFile } from '../function/functions'
+// nginx 실행 관련
+import { startNginxProcess } from '../function/nginx'
+import { quitNginxProcess }  from '../function/nginx'
+import { findNginxProcess }  from '../function/nginx'
+import { testNginxProcess }  from '../function/nginx'
+
+// functions 관련
+import { checkKey }          from '../function/functions'
+import { makeRTMPJSONFile }  from '../function/functions'
+import { makeNginxConfFile } from '../function/functions'
 
 export default {
   name: 'PageIndex',
+  
+  // 페이지가 마운트시 실행되는 메소드
   mounted() {
     window.addEventListener('load', () => {
       this.getSaveKey()
       this.turnOnSwitch()
     })
   },
+
+  // 페이지에서 사용되는 데이터 변수
   data () {
     return {
 
@@ -129,6 +152,24 @@ export default {
       toggleSwitchStatus: false
     }
   },
+
+  // 페이지에서 사용되는 메소드
+  /* 
+   * getSaveKey                     : rtmp.json 파일에서 RTMP URL 및 RTMP KEY 값을 가져옴
+   * turnOnSwitch                   : rtmp.json 파일에서 RTMP KEY가 존재하는 입력칸의 토글 스위치만 ON으로 세팅
+   * checkTwitckKeyNull             : TWITCH KEY 값이 NULL이 되었는지 확인
+   * checkYoutubeKeyNull            : YOUTUBE KEY 값이 NULL이 되었는지 확인
+   * checkAddtionalRTMPUrlOrKeyNull : 추가적인 RTMP URL 혹은 KEY 값이 NULL이 되었는지 확인
+   * makeNginxConf                  : nginx.conf 파일을 생성함
+   * makeRTMPJSON                   : rtmp.json 파일을 생성함
+   * nginxSwitch                    : nginx를 ON/OFF 함
+   * notify                         : 사용자에게 알림 메시지를 표시함
+   * checkIncludeKoreanOnPath       : 프로그램 경로에 한글이 존재하는지 확인
+   * toggleSwitchIsToggle           : nginx가 실행된 상태에서 토글 스위치의 상태가 변경되었는지 확인
+   * nginxIsWorking                 : nginx가 실행중인지 확인함
+   * nginxReload                    : nginx를 재시작함
+   * 
+   */
   methods: {
 
     // rtmp.json 파일에서 키 값을 가져와 세팅함
@@ -158,7 +199,7 @@ export default {
     },
 
     // twitchKey q-input 값이 비는 순간 스위치를 끔
-    checTwitchKeykNull (value) {
+    checkTwitchKeykNull (value) {
       if (!value) {
         this.twitchOn = false
       }
@@ -185,7 +226,7 @@ export default {
         youtube: this.youtubeKey.trim()
       }
 
-      let results = await checkKey(keys)
+      const results = await checkKey(keys)
 
       if (!(results.twitch && results.youtube)) {
         if (!results.twitch) { this.notify('negative', 'TWITCH KEY 값에 문제가 있습니다') }
@@ -272,7 +313,7 @@ export default {
     },
 
     // nginx.exe가 겨진 상태에서, Key 적용여부 토글 스위치 상태가 변했는지 체크함
-    toggleSwitch () {
+    toggleSwitchIsToggle () {
       if (this.$store.state.nginxStatus) {
         this.toggleSwitchStatus = true
       }
--- src/store/index.js
+++ src/store/index.js
@@ -20,10 +20,16 @@ export default function (/* { ssrContext } */) {
       // example
     },
     state: {
+      
+      // nginx가 켜져있으면 true, 꺼져있으면 false
       nginxStatus: false,
+
+      // build 완료된 결과물은 끝에 "\\resources\\app.asar"가 추가 경로로 붙으므로 지워야 함
       dir: __dirname.replace("\\resources\\app.asar", "")
     },
     mutations: {
+      
+      // nginxStatus 변수에 변경 사항을 적용하기 위한 메소드
       setNginxStatus: function (state, data) {
         state.nginxStatus = data
       }
