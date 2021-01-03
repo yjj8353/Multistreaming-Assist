102eb7a09001abf7dd217ebbe6eade9a875e9799
yjj8353 <yjj8353@gmail.com>
2021-01-03 21:12:02 +0900

    ShellMixin 메소드 추가

--- src/mixins/ShellMixin.ts
+++ src/mixins/ShellMixin.ts
@@ -2,6 +2,26 @@
  * Shell Command 관련 기능
  */
 
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
 import { execFile, execFileSync, exec, execSync } from 'child_process'
 
+@Component
+export class ShellMixin extends Vue {
+  useExecFile() {
 
+  }
+
+  useExecFileSync() {
+
+  }
+
+  useExec() {
+
+  }
+
+  useExecSync() {
+    
+  }
+}
