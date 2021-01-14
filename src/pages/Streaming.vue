07a254ea7237927ebcece7e743dce0f6cdb7e484
yjj8353 <yjj8353@gmail.com>
2021-01-14 16:26:42 +0900

    Streaming page 수정

--- src/pages/Streaming.vue
+++ src/pages/Streaming.vue
@@ -1,14 +1,105 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page class="q-pa-md" style="background-color: white;">
+
+    <!-- TWITCH RTMP KEY 입력 칸 -->
+    <div class="row">
+      <div class="col-11">
+        <q-input label="TWITCH RTMP KEY" v-model="twitchKey" :type="twitchIsPwd ? 'password' : 'text'" @input="checkTwitchKeykNull">
+          <template v-slot:append>
+            <q-icon
+              :name="twitchIsPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="twitchIsPwd = !twitchIsPwd"
+            />
+          </template>
+        </q-input>
+      </div>
+
+      <!-- TWITCH RTMP KEY 반영 토글 스위치 -->
+      <div class="col-1" style="display: flex; align-items: center; justify-content: center;">
+        <q-toggle v-model="twitchOn" :disable="!twitchKey" size="md" />
+      </div>
+    </div>
+
+    <p></p>
+
+    <!-- YOUTUBE RTMP KEY 입력 칸 -->
+    <div class="row">
+      <div class="col-11">
+        <q-input label="YOUTUBE RTMP KEY" v-model="youtubeKey" :type="youtubeIsPwd ? 'password' : 'text'" @input="checkYoutubeKeyNull">
+          <template v-slot:append>
+            <q-icon
+              :name="youtubeIsPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="youtubeIsPwd = !youtubeIsPwd"
+            />
+          </template>
+        </q-input>
+      </div>
+
+        <!-- YOUTUBE RTMP KEY 반영 토글 스위치 -->
+      <div class="col-1" style="display: flex; align-items: center; justify-content: center;">
+        <q-toggle v-model="youtubeOn" :disable="!youtubeKey" size="md" />
+      </div>
+    </div>
+
+    <p></p>
+
+    <!-- 추가적인 RTMP URL 입력 칸 -->
+    <div class="row">
+      <div class="q-pr-md col">
+        <q-input label="추가적인 RTMP URL" v-model="additionalRTMPUrl" type="text" @input="checkAddtionalRTMPUrlOrKeyNull">
+          <template v-slot:append>
+          </template>
+        </q-input>
+      </div>
+
+      <!-- 추가적은 RTMP KEY 입력 칸 -->
+      <div class="q-pl-md col">
+        <q-input label="추가적인 RTMP KEY" v-model="additionalRTMPKey" :type="additionalIsPwd ? 'password' : 'text'" @input="checkAddtionalRTMPUrlOrKeyNull">
+          <template v-slot:append>
+            <q-icon
+              :name="additionalIsPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="additionalIsPwd = !additionalIsPwd"
+            />
+          </template>
+        </q-input>
+      </div>
+
+        <!-- 추가적인 RTMP URL 및 KEY 반영 토글 스위치 -->
+      <div class="col-1" style="display: flex; align-items: center; justify-content: center;">
+        <q-toggle size="md"
+                  v-model="additionalOn"
+                  :disable="!additionalRTMPUrl || !additionalRTMPKey"
+        />
+      </div>
+    </div>
+
+    <p></p>
+
+    <!-- NGINX 시작/종료 버튼 -->
+    <div class="row">
+      <div class="q-pl-md col">
+        <q-btn size="lg"
+                class="float-right"
+                :color="nginxStatus ? 'red' : 'primary'"
+                :label="nginxStatus ? 'Nginx 종료' : 'Nginx 시작'"
+                @click="nginxSwitch"
+        />
+      </div>
+    </div>
+
   </q-page>
 </template>
 
 <script lang="ts">
+import { NginxMixin } from 'src/mixins/NiginxMixin';
+import { mixins } from 'vue-class-component';
 import { Vue, Component } from 'vue-property-decorator'
 
 @Component
-export default class StreamingPage extends Vue {
+export default class StreamingPage extends mixins(NginxMixin) {
   
 }
 </script>
